4881af0a1611b392440e4b3aef3a4514cc8fd550
EstebanGC <johan.gal.car@gmail.com>
2024-04-12 22:10:34 -0500

    .

--- src/main/java/com/mycompany/app/modelmapper/ModelMapperMain.java
+++ src/main/java/com/mycompany/app/modelmapper/ModelMapperMain.java
@@ -2,6 +2,6 @@ package com.mycompany.app.modelmapper;
 
 public class ModelMapperMain {
     public static void main(String[] args) {
-        System.out.println("Starting with Model Mapper");
+        System.out.println("Saying whatever!");
     }
 }
