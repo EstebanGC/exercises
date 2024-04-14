55b756000a6b2f6c95ade8141c9cb4e5056b24b8
EstebanGC <johan.gal.car@gmail.com>
2024-04-13 21:34:12 -0500

    .

--- src/main/java/com/mycompany/app/modelmapper/ModelMapperMain.java
+++ src/main/java/com/mycompany/app/modelmapper/ModelMapperMain.java
@@ -2,6 +2,7 @@ package com.mycompany.app.modelmapper;
 
 public class ModelMapperMain {
     public static void main(String[] args) {
-        System.out.println("Saying whatever!");
+        System.out.println("Saying whatever!" +
+                "Saying whatever2");
     }
 }
